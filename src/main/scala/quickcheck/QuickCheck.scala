134b9d1ba9daeee16c7af34ed4b2c12058ee80c2
artsiomchekh <qbartem@gmail.com>
2023-08-02 10:21:41 +0300

    added properties to fix bugs

--- src/main/scala/quickcheck/QuickCheck.scala
+++ src/main/scala/quickcheck/QuickCheck.scala
@@ -24,18 +24,23 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
     findMin(insert(m, h)) == m
   }
 
-  property("smallestOfTheTwoElements") = forAll { (a: Int, b: Int) =>
+  property("min1") = forAll { (a: Int) =>
+    val h = insert(a, empty)
+    findMin(h) == a
+  }
+
+  property("smallest of the two elements") = forAll { (a: Int, b: Int) =>
     val h = insert(b, insert(a, empty))
     val smallestElement = if (a > b) b else a
     findMin(h) == smallestElement
   }
 
-  property("emptyResultingHeap") = forAll { (a: Int) =>
+  property("empty heap after insert and delete") = forAll { (a: Int) =>
     val h = insert(a, empty)
     isEmpty(deleteMin(h))
   }
 
-  property("sortedSequenceOfElements") = forAll { (h: H) =>
+  property("sorted sequence of elements when continually finding and deleting minima") = forAll { (h: H) =>
     @tailrec
     def remainingMin(remaining: H, deleteList: List[Int]): List[Int] = remaining match
       case h if h == empty => Nil
@@ -59,7 +64,7 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
     contains(newHeap)
   }
 
-  property("minimumOfOneOrTheOtherHeaps") = forAll { (h1: H, h2: H) =>
+  property("minimum of one or the other heaps") = forAll { (h1: H, h2: H) =>
     if isEmpty(h1) && isEmpty(h2) then
       isEmpty(meld(h1, h2))
     else if isEmpty(h1) && !isEmpty(h2) then
@@ -73,6 +78,46 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap:
       val minMeldH = findMin(meldH)
       minMeldH == minH1 || minMeldH == minH2
   }
+
+  property("meld nonempty heap with empty heap equals first heap") = forAll { (a: Int) =>
+    val h1 = insert(a, empty)
+    val h2 = empty
+    val meldHeap = meld(h1, h2)
+    meldHeap == h1
+  }
+
+  property("meld empty heap with nonempty heap equals second heap") = forAll { (a: Int) =>
+    val h1 = empty
+    val h2 = insert(a, empty)
+    val meldHeap = meld(h1, h2)
+    meldHeap == h2
+  }
+
+  property("delete min 2 elems results empty") =
+    forAll { (a: Int, b: Int) =>
+      val h1 = insert(a, insert(b, empty))
+      deleteMin(deleteMin(h1)) == empty
+    }
+
+  property("meld two heaps then deleting and inserting min element of one of them") = forAll { (h1: H, h2: H) =>
+    @tailrec
+    def loop(h1: H, h2: H): Boolean = (h1, h2) match
+      case (Nil, Nil) => true
+      case _ => findMin(h1) == findMin(h2) && loop(deleteMin(h1), deleteMin(h2))
+
+    if isEmpty(h1) && isEmpty(h2) then true
+    else if isEmpty(h1) then loop(meld(h1, h2), meld(h1, h2))
+    else if isEmpty(h2) then loop(meld(h1, h2), meld(h1, h2))
+    else loop(meld(h1, h2), meld(deleteMin(h1), insert(findMin(h1), h2)))
+  }
+
+
+
+
+
+
+
+
 
 
 
